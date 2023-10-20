1f39f973c9e9b06613ad6630d6042f5ca34031bb
Jinwoo Lee <binaryWooo@gmail.com>
2023-10-20 18:53:39 +0900

    feat: server public ip 부여

--- main.tf
+++ main.tf
@@ -3,3 +3,7 @@ resource "ncloud_server" "server" {
     server_image_product_code = "SPSW0LINUX000130"
     server_product_code = "SPSVRSTAND000003"
 }
+
+resource "ncloud_public_ip" "public_ip" {
+  server_instance_no = "20131099"
+}
