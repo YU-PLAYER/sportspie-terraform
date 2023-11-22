1c67b0e5d2336748ea3ac5b93f4699d820b21c64
Jinwoo Lee <binaryWooo@gmail.com>
2023-11-23 06:47:09 +0900

    feat: implement db-server

--- main.tf
+++ main.tf
@@ -43,6 +43,12 @@ resource "ncloud_access_control_group" "was_acg_01" {
   vpc_no      = var.vpc_no
 }
 
+resource "ncloud_access_control_group" "db_acg_01" {
+  name        = "${var.terraform_name}-acg02"
+  description = "${var.terraform_name} Access controle group"
+  vpc_no      = var.vpc_no
+}
+
 resource "ncloud_access_control_group_rule" "web_acg_rule_01" {
   access_control_group_no = ncloud_access_control_group.web_acg_01.id
   
@@ -99,6 +105,34 @@ resource "ncloud_access_control_group_rule" "was_acg_rule_01" {
   }
 }
 
+resource "ncloud_access_control_group_rule" "db_acg_rule_01" {
+  access_control_group_no = ncloud_access_control_group.db_acg_01.id
+  
+  inbound {
+    protocol    = "TCP"
+    ip_block    = "0.0.0.0/0"
+    port_range  = "22"
+    description = "accept 22 port(all ip)"
+  }
+  outbound {
+    protocol    = "TCP"
+    ip_block    = "0.0.0.0/0" 
+    port_range  = "1-65535"
+    description = "accept TCP 1-65535 port"
+  }
+  outbound {
+    protocol    = "UDP"
+    ip_block    = "0.0.0.0/0" 
+    port_range  = "1-65535"
+    description = "accept UDP 1-65535 port"
+  }
+  outbound {
+    protocol    = "ICMP"
+    ip_block    = "0.0.0.0/0" 
+    description = "accept ICMP"
+  }
+}
+
 resource "ncloud_network_interface" "web_nic" {
   name                  = "${var.terraform_name}-web-nic"
   subnet_no             = var.sunbet_public_id
@@ -109,6 +143,12 @@ resource "ncloud_network_interface" "was_nic" {
   name                  = "${var.terraform_name}-was-nic"
   subnet_no             = var.sunbet_public_id
   access_control_groups = [ncloud_access_control_group.was_acg_01.id]
+}
+
+resource "ncloud_network_interface" "db_nic" {
+  name                  = "${var.terraform_name}-db-nic"
+  subnet_no             = var.sunbet_public_id
+  access_control_groups = [ncloud_access_control_group.db_acg_01.id]
 }
 
 resource "ncloud_server" "web_server" {
@@ -131,6 +171,18 @@ resource "ncloud_server" "was_server" {
     login_key_name = ncloud_login_key.was_key.key_name
     network_interface   {
       network_interface_no = ncloud_network_interface.was_nic.id
+      order = 0
+  }
+}
+
+resource "ncloud_server" "db_server" {
+    subnet_no = var.sunbet_public_id
+    name = "${var.terraform_name}-db"
+    server_image_product_code = "SW.VSVR.OS.LNX64.UBNTU.SVR2004.B050"
+    server_product_code = "SVR.VSVR.STAND.C002.M008.NET.HDD.B050.G002"
+    login_key_name = ncloud_login_key.was_key.key_name
+    network_interface   {
+      network_interface_no = ncloud_network_interface.db_nic.id
       order = 0
   }
 }
