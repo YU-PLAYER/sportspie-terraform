bb6c2d1bf4bd932ed942ab8e66db2c8c79791c4c
Jinwoo Lee <binaryWooo@gmail.com>
2023-11-24 21:22:51 +0900

    feat: implement one-server

--- main.tf
+++ main.tf
@@ -37,24 +37,6 @@ resource "ncloud_access_control_group" "sportspie_acg_01" {
   vpc_no      = var.vpc_no
 }
 
-resource "ncloud_access_control_group" "web_acg_01" {
-  name        = "${var.terraform_name}-acg00"
-  description = "${var.terraform_name} Access controle group"
-  vpc_no      = var.vpc_no
-}
-
-resource "ncloud_access_control_group" "was_acg_01" {
-  name        = "${var.terraform_name}-acg01"
-  description = "${var.terraform_name} Access controle group"
-  vpc_no      = var.vpc_no
-}
-
-resource "ncloud_access_control_group" "db_acg_01" {
-  name        = "${var.terraform_name}-acg02"
-  description = "${var.terraform_name} Access controle group"
-  vpc_no      = var.vpc_no
-}
-
 resource "ncloud_access_control_group_rule" "sportspie_acg_rule_01" {
   access_control_group_no = ncloud_access_control_group.sportspie_acg_01.id
   
@@ -101,136 +83,10 @@ resource "ncloud_access_control_group_rule" "sportspie_acg_rule_01" {
   }
 }
 
-resource "ncloud_access_control_group_rule" "web_acg_rule_01" {
-  access_control_group_no = ncloud_access_control_group.web_acg_01.id
-  
-  inbound {
-    protocol    = "TCP"
-    ip_block    = "0.0.0.0/0"
-    port_range  = "22"
-    description = "accept 22 port(all ip)"
-  }
-  inbound {
-    protocol    = "TCP"
-    ip_block    = "0.0.0.0/0"
-    port_range  = "80"
-    description = "accept 80 port(all ip)"
-  }
-  inbound {
-    protocol    = "TCP"
-    ip_block    = "0.0.0.0/0"
-    port_range  = "3000"
-    description = "accept 3000 port(all ip)"
-  }
-  outbound {
-    protocol    = "TCP"
-    ip_block    = "0.0.0.0/0" 
-    port_range  = "1-65535"
-    description = "accept TCP 1-65535 port"
-  }
-  outbound {
-    protocol    = "UDP"
-    ip_block    = "0.0.0.0/0" 
-    port_range  = "1-65535"
-    description = "accept UDP 1-65535 port"
-  }
-  outbound {
-    protocol    = "ICMP"
-    ip_block    = "0.0.0.0/0" 
-    description = "accept ICMP"
-  }
-}
-
-resource "ncloud_access_control_group_rule" "was_acg_rule_01" {
-  access_control_group_no = ncloud_access_control_group.was_acg_01.id
-  
-  inbound {
-    protocol    = "TCP"
-    ip_block    = "0.0.0.0/0"
-    port_range  = "22"
-    description = "accept 22 port(all ip)"
-  }
-  inbound {
-    protocol    = "TCP"
-    ip_block    = "0.0.0.0/0"
-    port_range  = "8080"
-    description = "accept 8080 port(all ip)"
-  }
-  outbound {
-    protocol    = "TCP"
-    ip_block    = "0.0.0.0/0" 
-    port_range  = "1-65535"
-    description = "accept TCP 1-65535 port"
-  }
-  outbound {
-    protocol    = "UDP"
-    ip_block    = "0.0.0.0/0" 
-    port_range  = "1-65535"
-    description = "accept UDP 1-65535 port"
-  }
-  outbound {
-    protocol    = "ICMP"
-    ip_block    = "0.0.0.0/0" 
-    description = "accept ICMP"
-  }
-}
-
-resource "ncloud_access_control_group_rule" "db_acg_rule_01" {
-  access_control_group_no = ncloud_access_control_group.db_acg_01.id
-  
-  inbound {
-    protocol    = "TCP"
-    ip_block    = "0.0.0.0/0"
-    port_range  = "22"
-    description = "accept 22 port(all ip)"
-  }
-  inbound {
-    protocol    = "TCP"
-    ip_block    = "0.0.0.0/0"
-    port_range  = "3306"
-    description = "accept 3306 port(all ip)"
-  }
-  outbound {
-    protocol    = "TCP"
-    ip_block    = "0.0.0.0/0" 
-    port_range  = "1-65535"
-    description = "accept TCP 1-65535 port"
-  }
-  outbound {
-    protocol    = "UDP"
-    ip_block    = "0.0.0.0/0" 
-    port_range  = "1-65535"
-    description = "accept UDP 1-65535 port"
-  }
-  outbound {
-    protocol    = "ICMP"
-    ip_block    = "0.0.0.0/0" 
-    description = "accept ICMP"
-  }
-}
-
 resource "ncloud_network_interface" "sportspie_nic" {
   name                  = "${var.terraform_name}-nic"
   subnet_no             = var.sunbet_public_id
-  access_control_groups = [ncloud_access_control_group.web_acg_01.id]
-}
-
-resource "ncloud_network_interface" "web_nic" {
-  name                  = "${var.terraform_name}-web-nic"
-  subnet_no             = var.sunbet_public_id
-  access_control_groups = [ncloud_access_control_group.web_acg_01.id]
-}
-
-resource "ncloud_network_interface" "was_nic" {
-  name                  = "${var.terraform_name}-was-nic"
-  subnet_no             = var.sunbet_public_id
-  access_control_groups = [ncloud_access_control_group.was_acg_01.id]
-}
-
-resource "ncloud_network_interface" "db_nic" {
-  name                  = "${var.terraform_name}-db-nic"
-  subnet_no             = var.sunbet_public_id
-  access_control_groups = [ncloud_access_control_group.db_acg_01.id]
+  access_control_groups = [ncloud_access_control_group.sportspie_acg_01.id]
 }
 
 resource "ncloud_server" "server" {
@@ -245,59 +101,8 @@ resource "ncloud_server" "server" {
   }
 }
 
-resource "ncloud_server" "web_server" {
-    subnet_no = var.sunbet_public_id
-    name = "${var.terraform_name}-web"
-    server_image_product_code = "SW.VSVR.OS.LNX64.UBNTU.SVR2004.B050"
-    server_product_code = "SVR.VSVR.STAND.C002.M008.NET.HDD.B050.G002"
-    login_key_name = ncloud_login_key.sportspie_key.key_name
-    network_interface   {
-      network_interface_no = ncloud_network_interface.web_nic.id
-      order = 0
-  }
-}
-
-resource "ncloud_server" "was_server" {
-    subnet_no = var.sunbet_public_id
-    name = "${var.terraform_name}-was"
-    server_image_product_code = "SW.VSVR.OS.LNX64.UBNTU.SVR2004.B050"
-    server_product_code = "SVR.VSVR.STAND.C002.M008.NET.HDD.B050.G002"
-    login_key_name = ncloud_login_key.sportspie_key.key_name
-    network_interface   {
-      network_interface_no = ncloud_network_interface.was_nic.id
-      order = 0
-  }
-}
-
-resource "ncloud_server" "db_server" {
-    subnet_no = var.sunbet_public_id
-    name = "${var.terraform_name}-db"
-    server_image_product_code = "SW.VSVR.OS.LNX64.UBNTU.SVR2004.B050"
-    server_product_code = "SVR.VSVR.STAND.C002.M008.NET.HDD.B050.G002"
-    login_key_name = ncloud_login_key.sportspie_key.key_name
-    network_interface   {
-      network_interface_no = ncloud_network_interface.db_nic.id
-      order = 0
-  }
-}
-
 data "ncloud_root_password" "sportspie_default" {
   server_instance_no = ncloud_server.server.instance_no
-  private_key = ncloud_login_key.sportspie_key.private_key
-}
-
-data "ncloud_root_password" "web_default" {
-  server_instance_no = ncloud_server.web_server.instance_no
-  private_key = ncloud_login_key.sportspie_key.private_key
-}
-
-data "ncloud_root_password" "was_default" {
-  server_instance_no = ncloud_server.was_server.instance_no
-  private_key = ncloud_login_key.sportspie_key.private_key
-}
-
-data "ncloud_root_password" "db_default" {
-  server_instance_no = ncloud_server.db_server.instance_no
   private_key = ncloud_login_key.sportspie_key.private_key
 }
 
@@ -306,37 +111,7 @@ resource "local_file" "sportspie_root_pw" {
   content = "${ncloud_server.server.name} => ${data.ncloud_root_password.sportspie_default.root_password}"
 }
 
-resource "local_file" "web_root_pw" {
-  filename = "${ncloud_server.web_server.name}-root_password.txt"
-  content = "${ncloud_server.web_server.name} => ${data.ncloud_root_password.web_default.root_password}"
-}
-
-resource "local_file" "was_root_pw" {
-  filename = "${ncloud_server.was_server.name}-root_password.txt"
-  content = "${ncloud_server.was_server.name} => ${data.ncloud_root_password.was_default.root_password}"
-}
-
-resource "local_file" "db_root_pw" {
-  filename = "${ncloud_server.db_server.name}-root_password.txt"
-  content = "${ncloud_server.db_server.name} => ${data.ncloud_root_password.db_default.root_password}"
-}
-
 resource "ncloud_public_ip" "server_ip" {
   server_instance_no = ncloud_server.server.id
   description        = "for ${ncloud_server.server.name} public ip"
-}
-
-resource "ncloud_public_ip" "web_ip" {
-  server_instance_no = ncloud_server.web_server.id
-  description        = "for ${ncloud_server.web_server.name} public ip"
-}
-
-resource "ncloud_public_ip" "was_ip" {
-  server_instance_no = ncloud_server.was_server.id
-  description        = "for ${ncloud_server.was_server.name} public ip"
-}
-
-resource "ncloud_public_ip" "db_ip" {
-  server_instance_no = ncloud_server.db_server.id
-  description        = "for ${ncloud_server.db_server.name} public ip"
 }
